d628b2a84c597c54e31a4e902d5a2b1174491b02
Erik Szigeti <erik.szigeti01@gmail.com>
2023-07-11 21:43:33 +0200

    Added test route to dummy endpoint

--- src/main/java/com/eszigeti/taskmanagerservice/controller/TaskController.java
+++ src/main/java/com/eszigeti/taskmanagerservice/controller/TaskController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TaskController {
 
-    @GetMapping
+    @GetMapping("test")
     String dummyEndpoint() {
         log.info("Dummy endpoint");
         return "OK";
